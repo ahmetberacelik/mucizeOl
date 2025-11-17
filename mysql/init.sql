fb51839e2df6d5afb821d1f55e8d1074cb4e7a3b
Ahmet Bera Çelik <ahmetberacelik@hotmail.com>
2025-11-17 20:16:50 +0300

    Docker compose ve Dockerfile yapılandırması yapıldı. Flyway migration ile database tablolarının otomatik eklenmesi sağlandı

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,8 +1,10 @@
 -- MySQL Veritabanı Oluşturma Scripti
 -- Bu script, Docker container ilk kez ayağa kalkarken çalıştırılır.
 
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+-- Veritabanını mucizeol_db değeriyle oluştur
+CREATE DATABASE IF NOT EXISTS `mucizeol_db` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+CREATE USER IF NOT EXISTS 'mucizeol_user'@'%' IDENTIFIED BY 's0m3_s3cur3_4pp_p4ssw0rd';
+GRANT ALL PRIVILEGES ON `mucizeol_db`.* TO 'mucizeol_user'@'%';
 
 FLUSH PRIVILEGES;
